86bed59d6092ba1cd8f1e330464fb00c7869d6e3
Gherman Marina <gherman.marina@icloud.com>
2020-03-03 11:57:22 +0100

    homework

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let startBtn = document.getElementById('start'),
+
+    budget = document.getElementsByClassName('budget-value'),
+    daybudget = document.getElementsByClassName('daybudget-value'),
+    level = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    income = document.getElementsByClassName('income-value'),
+    monthsavings = document.getElementsByClassName('monthsavings-value'),
+    yearsavings = document.getElementsByClassName('yearsavings-value'),
+    expensesItem = document.getElementsByClassName('expenses-item'),
+
+    expensesBtn = document.getElementsByTagName('button'),
+    countBtn = document.getElementsByTagName('button'),
+    optionalExpensesBtn = document.getElementsByTagName('button'),
+
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('.checksavings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
+    
+    
